2192645684c979faf21367538084400f32d2af04
tshamalamike@gmail.com <tshamalamike@gmail.com>
2022-07-30 10:51:42 +0200

    my first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-spacer></v-spacer>
+
+      <v-btn href="" text>
+        <span class="mr-2">Navbar</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-5" cols="12">
+        <v-data-table
+          :headers="headers"
+          :items="info"
+          :items-per-page="5"
+          class="elevation-1"
+        ></v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    headers: [
+      {
+        text: "categories",
+        align: "start",
+        sortable: false,
+        value: "value",
+      },
+    ],
+    info: [],
+  }),
+  mounted() {
+    axios
+      .get("https://api.chucknorris.io/jokes/search?query=all")
+      .then((response) => {
+        this.info = response.data.result;
+        console.log(response.data.result);
+      });
+  },
+};
+</script>
